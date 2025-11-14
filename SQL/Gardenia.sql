8027f5ab9f840fefa47cef707bd1459aefd76eff
Rafael Toledo Nunes <rafael.nunes1724@gmail.com>
2025-11-14 10:46:35 -0300

    Refactor SQL schema and update stored procedures
    Updated table structures and constraints for Endereco, Cliente, CategoriaProduto, Produto, Pedido, Pagamento, ItensCarrinho, and ItensPedidos. Modified stored procedures to include additional checks and return values.

--- SQL/Gardenia.sql
+++ SQL/Gardenia.sql
@@ -8,12 +8,14 @@ CREATE TABLE Endereco(
 ID_Endereco INT PRIMARY KEY IDENTITY,
 Rua VARCHAR(86) NOT NULL,
 Numero INT NOT NULL,
-Complemento VARCHAR(20) UNIQUE NULL,
+Complemento VARCHAR(20) NULL,
 Bairro VARCHAR(45) NOT NULL,
 CEP VARCHAR(9) NOT NULL,
 Municipio VARCHAR(45) NOT NULL,
-Estado CHAR(2) NOT NULL
+Estado CHAR(2) NOT NULL,
+CONSTRAINT UQ_Endereco UNIQUE (Rua, Numero, Complemento, Bairro, CEP, Municipio, Estado)
 );
+
 
 
 
@@ -29,45 +31,63 @@ ID_Endereco INT CONSTRAINT F_K_Endereco FOREIGN KEY(ID_Endereco) REFERENCES Ende
 );
 
 
+CREATE TABLE CategoriaProduto(
+ID_CategoriaProduto INT PRIMARY KEY IDENTITY,
+NomeCategoria VARCHAR(56) NOT NULL
+);
+
+
 CREATE TABLE Produto(
 ID_Produto INT PRIMARY KEY IDENTITY,
 Nome VARCHAR(78) NOT NULL,
 Preco DECIMAL(10,2)  NOT NULL,
-CategoriaProduto VARCHAR(75) NOT NULL CHECK(CategoriaProduto IN('Bolsas e mochilas','Acessorios de moda','Roupas em Croche'))
+ID_CategoriaProduto INT CONSTRAINT F_K_CategoriaProduto FOREIGN KEY(ID_CategoriaProduto) REFERENCES CategoriaProduto(ID_CategoriaProduto)
 );
+
 
 
 
 CREATE TABLE Pedido(
 ID_Pedido INT PRIMARY KEY IDENTITY,
 DataHoraPedido DATETIME NOT NULL DEFAULT GETDATE(),
-StatusPagamento VARCHAR(25) NOT NULL CHECK (StatusPagamento IN('Pagamento Aprovado','Pagamento Não Aprovado','Pagamento Pendente')),
+StatusPedido VARCHAR(25) NOT NULL CHECK (StatusPedido IN('Aguardando Pagamento','Concluido','Em Transporte','Enviado','Cancelado')),
 ID_Cliente INT CONSTRAINT F_K_Cliente FOREIGN KEY(ID_Cliente) REFERENCES Cliente(ID_Cliente)
 );
 
 
+CREATE TABLE Pagamento(
+ID_Pagamento INT PRIMARY KEY IDENTITY,
+StatusPagamento VARCHAR(35) CHECK (StatusPagamento IN ('Pendente', 'Aprovado', 'Recusado','Estornado')),
+FormaPagamento VARCHAR(25),
+ValorGasto DECIMAL(10,2),
+DataHora DATETIME DEFAULT GETDATE(),
+ID_Pedido INT CONSTRAINT F_K_Pedido FOREIGN KEY(ID_Pedido) REFERENCES Pedido(ID_Pedido)
+);
+
+
+
 
 CREATE TABLE ItensCarrinho (
-  ID_ItemCarrinho INT IDENTITY PRIMARY KEY,
-  ID_Cliente INT NOT NULL,
-  ID_Produto INT NOT NULL,
-  Quantidade INT NOT NULL DEFAULT 1 CHECK (Quantidade > 0),
-  PrecoUnitario DECIMAL(10,2) NOT NULL CHECK (PrecoUnitario >= 0),
-  CONSTRAINT FK_ItensCarrinho_Cliente FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente),
-  CONSTRAINT FK_ItensCarrinho_Produto FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto)
+ID_ItemCarrinho INT IDENTITY PRIMARY KEY,
+ID_Cliente INT NOT NULL,
+ID_Produto INT NOT NULL,
+Quantidade INT NOT NULL DEFAULT 1 CHECK (Quantidade > 0),
+PrecoUnitario DECIMAL(10,2) NOT NULL CHECK (PrecoUnitario >= 0),
+CONSTRAINT FK_ItensCarrinho_Cliente FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente),
+CONSTRAINT FK_ItensCarrinho_Produto FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto)
 );
 
 
 
 CREATE TABLE ItensPedidos (
-  ID_ItensPedidos INT PRIMARY KEY IDENTITY,
-  ID_Pedido INT NOT NULL,
-  ID_Produto INT NOT NULL,
-  Quantidade INT NOT NULL DEFAULT 1,
-  PrecoUnitario DECIMAL(10,2) NOT NULL,
-  StatusProduto VARCHAR(25) NOT NULL CHECK (StatusProduto IN('Pedido Confirmado','Em Transporte','Entregue','Cancelado','Devolvido')),
-  CONSTRAINT F_K_ItensPedidos_Pedido FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido),
-  CONSTRAINT F_K_ItensPedidos_Produtos FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto)
+ID_ItensPedidos INT PRIMARY KEY IDENTITY,
+ID_Pedido INT NOT NULL,
+ID_Produto INT NOT NULL,
+Quantidade INT NOT NULL DEFAULT 1,
+PrecoUnitario DECIMAL(10,2) NOT NULL,
+StatusProduto VARCHAR(25) NOT NULL CHECK (StatusProduto IN('Pedido Confirmado','Em Transporte','Entregue','Cancelado','Devolvido')),
+CONSTRAINT F_K_ItensPedidos_Pedido FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido),
+CONSTRAINT F_K_ItensPedidos_Produtos FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto)
 );
 
 
@@ -151,7 +171,6 @@ END;
 
 
 
-
 CREATE PROCEDURE CadastrarEndereco
     @Rua VARCHAR(86),
     @Numero INT,
@@ -162,14 +181,15 @@ CREATE PROCEDURE CadastrarEndereco
     @Estado CHAR(2)
 AS 
 BEGIN
-  IF EXISTS(SELECT 1 FROM Endereco WHERE CEP = @CEP AND Rua = @Rua AND Numero = @Numero)
+  IF EXISTS(SELECT 1 FROM Endereco WHERE CEP = @CEP AND Rua = @Rua AND Numero = @Numero AND Bairro = @Bairro)
     BEGIN
         RAISERROR('Endereço já cadastrado.', 16, 1);
-        RETURN;
+        RETURN 1;
     END
 
     INSERT INTO Endereco (Rua, Numero, Complemento, Bairro, CEP, Municipio, Estado)
     VALUES (@Rua, @Numero, @Complemento, @Bairro, @CEP, @Municipio, @Estado);
+    RETURN 0;
 END;
 
 
@@ -197,11 +217,12 @@ BEGIN
 IF EXISTS(SELECT 1 FROM Cliente WHERE CPF = @CPF)
     BEGIN
     RAISERROR('Cliente já cadastrado com este CPF.', 16, 1);
-    RETURN;
+    RETURN 1;
   END
 
 INSERT INTO Cliente(NomeCompleto, Email, HashSenha, AlgoSenha, Telefone, CPF, ID_Endereco)
-VALUES(@NomeCompleto, @Email, @HashSenha, @AlgoSenha, @Telefone, @CPF, @ID_Endereco)
+VALUES(@NomeCompleto, @Email, @HashSenha, @AlgoSenha, @Telefone, @CPF, @ID_Endereco);
+RETURN 0;
 END;
 
 
@@ -214,9 +235,8 @@ END;
 
 
 
-
 CREATE PROCEDURE CadastrarPedido
-@StatusPagamento VARCHAR(25),
+@StatusPedido VARCHAR(25),
 @ID_Cliente INT
 
 AS
@@ -224,10 +244,11 @@ BEGIN
 IF NOT EXISTS(SELECT 1 FROM Cliente WHERE ID_Cliente = @ID_Cliente)
 BEGIN
 RAISERROR('Não é possivel encontrar o cliente', 16,1)
-RETURN;
+RETURN 1;
 END
 
 
-INSERT INTO Pedido(StatusPagamento, ID_Cliente) VALUES
-(@StatusPagamento, @ID_Cliente)
+INSERT INTO Pedido(StatusPedido, ID_Cliente) VALUES
+(@StatusPedido, @ID_Cliente);
+RETURN 0;
 END;
