70eadf3a3d71436a62a97e3d1b3bce8b5ab16987
Rafael-Nunes18 <rafael.nunes1724@gmail.com>
2025-12-11 08:32:53 -0300

    Atualizando Front

--- obj/Debug/net9.0/Gardenia MVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gardenia MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gardenia MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9d68182cad80840806dc9f918072b865937535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c226515c97931d1e615e28b29d05fdfa1647d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gardenia MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gardenia MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
